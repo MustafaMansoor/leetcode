c2969bdcf50b7de7d5ac5b3fbee7168befd241b2
Mustafa Mansoor <mustafamansoor47@gmail.com>
2025-01-11 20:49:36 +0500

    solved 1679

---
+++ 1679. Max Number of K-Sum Pairs.py
@@ -0,0 +1,17 @@
+from typing import List
+
+class Solution:
+    def maxOperations(self, nums: List[int], k: int) -> int:
+        count = 0
+        numMap = {} 
+        
+        for num in nums:
+            target = k - num
+            if target in numMap and numMap[target] > 0:
+                count += 1
+                numMap[target] -= 1
+            else:
+                numMap[num] = numMap.get(num, 0) + 1
+        return count
+sol = Solution()
+print(sol.maxOperations([1,2,3,4], 5))
